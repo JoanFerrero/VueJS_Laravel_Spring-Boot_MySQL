2d3499aa8a76c5db9a6bf85c436e9bbd1cac4025
JoanFerrero <joan.ferrero.montiel@gmail.com>
2023-11-29 19:49:52 +0100

    End backend filters SpringBoot

--- backend/springboot/src/main/java/com/app/spring/datajpa/controller/MesaController.java
+++ backend/springboot/src/main/java/com/app/spring/datajpa/controller/MesaController.java
@@ -31,15 +31,75 @@ public class MesaController {
 		try {
 			mesaQueryParam.setName_mesa(mesaQueryParam.getName_mesa() + '%');
 			List<Mesa> mesas = new ArrayList<Mesa>();
-			if (mesaQueryParam.getCategories().length > 0) {
-				mesaRepository
-				.findCategoriesOnMesa(mesaQueryParam.getCategories(), mesaQueryParam.getName_mesa())
-				.forEach(mesas::add);
+			Integer offset = (mesaQueryParam.getPage() - 1) * mesaQueryParam.getLimit();
+
+			//Only Category
+			if (mesaQueryParam.getCategories().length > 0 && mesaQueryParam.getOrder() == 0 && mesaQueryParam.getCapacity() == 0) {
+				mesaRepository.findCategoriesOnMesa(mesaQueryParam.getCategories(), mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+
+			//Only Capacity
+			} else if(mesaQueryParam.getCapacity() > 0 && mesaQueryParam.getCategories().length == 0 && mesaQueryParam.getOrder() == 0){
+				mesaRepository.findCapasityMesa(mesaQueryParam.getCapacity(), mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+
+			//Only Order
+			} else if(mesaQueryParam.getCategories().length == 0 && mesaQueryParam.getCapacity() == 0 && mesaQueryParam.getOrder() > 0) {
+				if(mesaQueryParam.getOrder() == 1) {
+					mesaRepository.findOrderASC(mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+				} else {
+					mesaRepository.findOrderDESC( mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+				}
+
+			//Capacity with Order
+			} else if(mesaQueryParam.getCategories().length == 0 && mesaQueryParam.getCapacity() > 0 && mesaQueryParam.getOrder() > 0) {
+				if(mesaQueryParam.getOrder() == 1) {
+					mesaRepository.findCapacityASC(mesaQueryParam.getCapacity(), mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+				} else {
+					mesaRepository.findCapacityDESC(mesaQueryParam.getCapacity(), mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+				}
+
+			//Category with Order
+			} else if(mesaQueryParam.getCategories().length == 0 && mesaQueryParam.getCapacity() == 0 && mesaQueryParam.getOrder() > 0) {
+				if(mesaQueryParam.getOrder() == 1) {
+					mesaRepository.findCategoriesASC(mesaQueryParam.getCategories(), mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+				} else {
+					mesaRepository.findCategoriesDESC(mesaQueryParam.getCategories(), mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+				}
+
+			//category with capacity
+			} else if(mesaQueryParam.getCategories().length > 0 && mesaQueryParam.getCapacity() > 0 && mesaQueryParam.getOrder() == 0) {
+				mesaRepository.findCategoryWithCapacityOnMesa(mesaQueryParam.getCategories(), mesaQueryParam.getCapacity(), mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+
+			//category with capacity and order
+			} else if(mesaQueryParam.getCategories().length > 0 && mesaQueryParam.getCapacity() > 0 && mesaQueryParam.getOrder() > 0) {
+				mesaRepository.findCategoryWithCapacityOnMesa(mesaQueryParam.getCategories(), mesaQueryParam.getCapacity(), mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
+			
+			//All Mesa	
 			} else {
-				mesaRepository.findActive(mesaQueryParam.getName_mesa()).forEach(mesas::add);
+				mesaRepository.findActive(mesaQueryParam.getName_mesa(), mesaQueryParam.getLimit(), offset).forEach(mesas::add);
 			}
 
 			return new ResponseEntity<>(mesas, HttpStatus.OK);
+		} catch (Exception e) {
+			System.err.println(e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/mesaPaginate")
+	public ResponseEntity<Integer> getAllMesasPagination(@ModelAttribute MesaQueryParam mesaQueryParam) {
+		try {
+			mesaQueryParam.setName_mesa(mesaQueryParam.getName_mesa() + '%');
+			Integer total = 0;
+			if(mesaQueryParam.getCategories().length == 0 && mesaQueryParam.getCapacity() > 0){
+				total = mesaRepository.findByCapacityPaginate(mesaQueryParam.getCapacity(), mesaQueryParam.getName_mesa());
+			} else if (mesaQueryParam.getCategories().length > 0 && mesaQueryParam.getCapacity() == 0) {
+				total = mesaRepository.findCategoriesOnMesaPaginate(mesaQueryParam.getCategories(), mesaQueryParam.getName_mesa());
+			} else if (mesaQueryParam.getCategories().length > 0 && mesaQueryParam.getCapacity() > 0){
+				total = mesaRepository.findByCapacityAndCategoriesPaginate(mesaQueryParam.getCapacity(),mesaQueryParam.getCategories(), mesaQueryParam.getName_mesa());
+			} else {
+				total = mesaRepository.findActivePaginate(mesaQueryParam.getName_mesa());
+			}
+			return new ResponseEntity<>(total, HttpStatus.OK);
 		} catch (Exception e) {
 			System.err.println(e);
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -54,6 +114,20 @@ public class MesaController {
 			return new ResponseEntity<>(mesaData.get(), HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@GetMapping("/mesaInfinite")
+	public ResponseEntity<List<Mesa>> getGigMesas(@ModelAttribute MesaQueryParam mesaQueryParam) {
+		try {
+			List<Mesa> mesas = new ArrayList<Mesa>();
+			Integer limit = mesaQueryParam.getPage() * mesaQueryParam.getLimit();
+			mesaRepository.findBigTables(limit).forEach(mesas::add);
+			return new ResponseEntity<>(mesas, HttpStatus.OK);
+
+		} catch (Exception e) {
+			System.err.println(e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
--- backend/springboot/src/main/java/com/app/spring/datajpa/repository/MesaRepository.java
+++ backend/springboot/src/main/java/com/app/spring/datajpa/repository/MesaRepository.java
@@ -10,11 +10,69 @@ import com.app.spring.datajpa.model.Mesa;
 
 public interface MesaRepository extends JpaRepository<Mesa, Long> {
 
+        // Filters //
+
         // Only active
-        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE AND m.name_mesa LIKE :name_mesa ", nativeQuery = true)
-        List<Mesa> findActive(@Param("name_mesa") String name_mesa);
+        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE AND m.name_mesa LIKE :name_mesa LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findActive(@Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
 
         // Only categories
-        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE AND m.id IN( SELECT a.mesa_id FROM ( SELECT mc.mesa_id, COUNT(mc.category_id) as count_cat_id FROM mesas_categories mc LEFT JOIN categories c ON mc.category_id = c.id WHERE c.name_category IN :categories GROUP BY mc.mesa_id) AS a WHERE a.count_cat_id >= ALL ( SELECT b.count_cat FROM ( SELECT COUNT(*) as count_cat FROM categories cc WHERE cc.name_category IN :categories ) b ) ) AND m.name_mesa LIKE :name_mesa GROUP BY m.id LIMIT 9 OFFSET 0", nativeQuery = true)
-        List<Mesa> findCategoriesOnMesa(@Param("categories") String[] categories, @Param("name_mesa") String name_mesa);
+        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE AND m.id IN( SELECT a.mesa_id FROM ( SELECT mc.mesa_id, COUNT(mc.category_id) as count_cat_id FROM mesas_categories mc LEFT JOIN categories c ON mc.category_id = c.id WHERE c.name_category IN :categories GROUP BY mc.mesa_id) AS a WHERE a.count_cat_id >= ALL ( SELECT b.count_cat FROM ( SELECT COUNT(*) as count_cat FROM categories cc WHERE cc.name_category IN :categories ) b ) ) AND m.name_mesa LIKE :name_mesa GROUP BY m.id LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findCategoriesOnMesa(@Param("categories") String[] categories, @Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
+
+        // BigestTables
+        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE ORDER BY m.capacity DESC LIMIT :limit", nativeQuery = true)
+        List<Mesa> findBigTables(@Param("limit") Integer limit);
+
+        //Only Capacity
+        @Query(value = "SELECT * FROM mesas m WHERE m.capacity = :capacity AND m.is_active IS TRUE AND m.name_mesa LIKE :name_mesa LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findCapasityMesa(@Param("capacity") Number capacity, @Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
+
+        //Capacity ASC
+        @Query(value = "SELECT * FROM mesas m WHERE m.capacity = :capacity AND m.is_active IS TRUE AND m.name_mesa LIKE :name_mesa ORDER BY m.capacity ASC LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findCapacityASC(@Param("capacity") Number capacity, @Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
+ 
+        //Capacity DESC
+        @Query(value = "SELECT * FROM mesas m WHERE m.capacity = :capacity AND m.is_active IS TRUE AND m.name_mesa LIKE :name_mesa ORDER BY m.capacity DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findCapacityDESC(@Param("capacity") Number capacity, @Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
+
+        //Only order ASC
+        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE AND m.name_mesa LIKE :name_mesa ORDER BY m.capacity ASC LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findOrderASC(@Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
+        
+        //Only order DESC
+        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE AND m.name_mesa LIKE :name_mesa ORDER BY m.capacity DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findOrderDESC(@Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
+
+        // Category with Capacity
+        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE AND m.id IN( SELECT a.mesa_id FROM ( SELECT mc.mesa_id, COUNT(mc.category_id) as count_cat_id FROM mesas_categories mc LEFT JOIN categories c ON mc.category_id = c.id WHERE c.name_category IN :categories GROUP BY mc.mesa_id) AS a WHERE a.count_cat_id >= ALL ( SELECT b.count_cat FROM ( SELECT COUNT(*) as count_cat FROM categories cc WHERE cc.name_category IN :categories ) b ) ) AND m.capacity = :capacity AND m.name_mesa LIKE :name_mesa GROUP BY m.id LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findCategoryWithCapacityOnMesa(@Param("categories") String[] categories, @Param("capacity") Number capacity, @Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
+
+        // Categories ASC
+        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE AND m.id IN( SELECT a.mesa_id FROM ( SELECT mc.mesa_id, COUNT(mc.category_id) as count_cat_id FROM mesas_categories mc LEFT JOIN categories c ON mc.category_id = c.id WHERE c.name_category IN :categories GROUP BY mc.mesa_id) AS a WHERE a.count_cat_id >= ALL ( SELECT b.count_cat FROM ( SELECT COUNT(*) as count_cat FROM categories cc WHERE cc.name_category IN :categories ) b ) ) AND m.name_mesa LIKE :name_mesa GROUP BY m.id ORDER BY m.capacity ASC LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findCategoriesASC(@Param("categories") String[] categories, @Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
+
+        // Categories DESC
+        @Query(value = "SELECT * FROM mesas m WHERE m.is_active IS TRUE AND m.id IN( SELECT a.mesa_id FROM ( SELECT mc.mesa_id, COUNT(mc.category_id) as count_cat_id FROM mesas_categories mc LEFT JOIN categories c ON mc.category_id = c.id WHERE c.name_category IN :categories GROUP BY mc.mesa_id) AS a WHERE a.count_cat_id >= ALL ( SELECT b.count_cat FROM ( SELECT COUNT(*) as count_cat FROM categories cc WHERE cc.name_category IN :categories ) b ) ) AND m.name_mesa LIKE :name_mesa GROUP BY m.id ORDER BY m.capacity DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
+        List<Mesa> findCategoriesDESC(@Param("categories") String[] categories, @Param("name_mesa") String name_mesa, @Param("limit") Integer limit, @Param("offset") Integer offset);
+        
+        // Pagination //
+
+        // Only active
+        @Query(value = "SELECT COUNT(*) FROM mesas m WHERE m.is_active IS TRUE AND m.name_mesa LIKE :name_mesa", nativeQuery = true)
+        Integer findActivePaginate(@Param("name_mesa") String name_mesa);
+
+        // Only categories
+        @Query(value = "SELECT COUNT(*) FROM mesas m WHERE m.is_active IS TRUE AND m.id IN( SELECT a.mesa_id FROM ( SELECT mc.mesa_id, COUNT(mc.category_id) as count_cat_id FROM mesas_categories mc LEFT JOIN categories c ON mc.category_id = c.id WHERE c.name_category IN :categories GROUP BY mc.mesa_id) AS a WHERE a.count_cat_id >= ALL ( SELECT b.count_cat FROM ( SELECT COUNT(*) as count_cat FROM categories cc WHERE cc.name_category IN :categories ) b ) ) AND m.name_mesa LIKE :name_mesa", nativeQuery = true)
+        Integer findCategoriesOnMesaPaginate(@Param("categories") String[] categories,
+                @Param("name_mesa") String name_mesa);
+
+        // Only capacity
+        @Query(value = "SELECT COUNT(*) FROM mesas m WHERE m.capacity = :capacity AND m.is_active IS TRUE AND m.name_mesa LIKE :name_mesa", nativeQuery = true)
+        Integer findByCapacityPaginate(@Param("capacity") Number capacity, @Param("name_mesa") String name_mesa);
+
+        // Capacity and categories
+        @Query(value = "SELECT COUNT(*) FROM mesas m WHERE m.is_active IS TRUE AND m.id IN( SELECT a.mesa_id FROM ( SELECT mc.mesa_id, COUNT(mc.category_id) as count_cat_id FROM mesas_categories mc LEFT JOIN categories c ON mc.category_id = c.id WHERE c.name_category IN :categories GROUP BY mc.mesa_id) AS a WHERE a.count_cat_id >= ALL ( SELECT b.count_cat FROM ( SELECT COUNT(*) as count_cat FROM categories cc WHERE cc.name_category IN :categories ) b ) ) AND m.capacity = :capacity AND m.name_mesa LIKE :name_mesa", nativeQuery = true)
+        Integer findByCapacityAndCategoriesPaginate(@Param("capacity") Number capacity,
+                @Param("categories") String[] categories, @Param("name_mesa") String name_mesa);
 }
