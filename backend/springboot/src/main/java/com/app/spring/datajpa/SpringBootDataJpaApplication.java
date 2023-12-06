ac088bfe5ddf8d9fc563331874629541faf58b13
JoanFerrero <joan.ferrero.montiel@gmail.com>
2023-12-06 20:20:58 +0100

    End login and register client

--- backend/springboot/src/main/java/com/app/spring/datajpa/SpringBootDataJpaApplication.java
+++ backend/springboot/src/main/java/com/app/spring/datajpa/SpringBootDataJpaApplication.java
@@ -2,8 +2,9 @@ package com.app.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
